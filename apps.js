342de7d794f35bc93cf66b8ce27425301fd95cbe
akhy4406019 <149380498+akhy4406019@users.noreply.github.com>
2024-01-24 00:09:24 +0600

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,73 @@
+// var sname='Akhy';
+// sname='Rimi';
+// document.write(sname);
+
+
+// const x=50;
+// x=69;
+// console.log(x);
+
+
+// var x=88;
+// var y=2;
+// var total=x+y;
+// var diffrent=x-y;
+// var div=x/y;
+// var mul=x*y;
+// x++;
+// document.write(x);
+// var multiple=x*y;
+//  document.write(multiple);
+
+
+// var x=22;
+// var y=2;
+// var total=x+y;
+// var diffrent=x-y;
+// var div=x/y;
+// var mul=x*y;
+// x**y;
+// document.write(x);
+
+
+// var x=10;
+// var y=3;
+// var remainder=x%y;
+// document.write(remainder);
+
+
+// var x=40;
+// var sname='Akhy Akter Rimi';
+// var sname=true;
+// document.write(typeof(sname));
+
+
+// var students=['Akhy','Rimi','Saima','Mohona','Trina','Asifa'];
+// document.write(typeof(students));
+// document.write(students[2]);
+
+
+// function myFunction(x,y){
+//     var result=x+y+'<br>'+ x*y;
+//     return result;
+// }
+// document.write(myFunction(28,65));
+
+
+// function myFunction(x,y){
+//      var result=x+y+'<br>'+ x*y;
+//      document.write(result);
+// }
+// myFunction(34,20);
+
+
+// var mainNumber=Number(prompt('Type Your Number'));
+// document.write(Math.pow(mainNumber,3));
+
+
+// var ran=Math.round(Math.random()*90000+5);
+// document.write(ran);
+
+
+var ran=Math.floor(Math.random()*90000+5);
+document.write(ran);
